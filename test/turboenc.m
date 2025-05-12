8f9dc715d68c7034f675bb6cf9843cdcdb190e2a
Gurx24 <17633837037@163.com>
2025-05-12 14:58:10 +0800

    initial commit

---
+++ test/turboenc.m
@@ -0,0 +1,11 @@
+%%%%%%%%%%%%%%%%%%%%%% turboenc.m %%%%%%%%%%%%%%%%%%%%%
+%
+% This program realized turrbo encoding.
+% 
+% date:2025.5.12  Gurx
+%
+
+clc; clear; close all;
+
+%******************** Parameters part *******************%
+
